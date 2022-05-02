c17bd0506caf4e6fceab1e61d0b6646407b9a8f9
gururajkd <guru.duggound@gmail.com>
2022-05-02 05:24:25 -0500

    test comited

---
+++ test.py
@@ -0,0 +1,37 @@
+print("hello")
+name=input("enter your name")
+print("name is",name)
+a=10
+b="guru"
+c=11.00
+print(type(a))
+print(type(b))
+print(type(c))
+a=b=c="selamio"
+print(a)
+print(b)
+print(c)
+text="hi how are you"
+print("hi" in text)
+b="king"
+print(b.replace("k","i"))
+fname="guru"
+lname="dugond"
+fullname=fname+lname
+print("fullname")
+print(b.lower())
+print(b.upper())
+print(b[2:6])
+print(b[1])
+print(b[:3])
+print(len(b))
+import random
+print(random.randrange(6,9))
+x=10
+y=20
+z=x+y
+print(z)
+print(x<y)
+print(x<y and y>x)
+print(x>y or y>x)
+
